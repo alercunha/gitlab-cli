57ed8e9e63d6e0cd370b7c35b8f99f1177d15a30
Alex Cunha <alexandre.cunha@gmail.com>
2017-10-04 17:21:54 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('gitlabcli/__init__.py', 'r') as fh:
 
 
 setup(
-    name='gitlabcli',
+    name='gitlab-cli',
     version=version,
     description='CLI tool for gitlab API',
     author='Alexandre Cunha',
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'requests>=2.0',
     ],
-    entry_points={'console_scripts': ['gitlabcli = gitlabcli:run']},
+    entry_points={'console_scripts': ['gitlab-cli = gitlabcli.run:main']},
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
