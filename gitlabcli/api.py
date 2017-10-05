74dd5bc8f7fb091f8a777db256b773d6484fa993
Alex Cunha <alexandre.cunha@gmail.com>
2017-10-05 10:19:19 -0500

    Further development

--- gitlabcli/api.py
+++ gitlabcli/api.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def _repo_url(project: str):
-    return '/projects/{0}/repository'.format(project.replace('/', '%2F'))
+def _project_url(project: str):
+    return '/projects/{0}'.format(project.replace('/', '%2F'))
 
 
 class GitlabAPI:
@@ -23,11 +23,20 @@ class GitlabAPI:
         return r.json()
 
     def commits(self, project: str, ref_name=None, since=None, until=None):
-        return self._get(_repo_url(project) + '/commits', ref_name=ref_name, since=since, until=until)
+        return self._get(_project_url(project) + '/repository/commits', ref_name=ref_name, since=since, until=until)
 
     def tags(self, project: str, name: str=None):
-        url = _repo_url(project) + '/tags'
+        url = _project_url(project) + '/repository/tags'
         if name:
             url = url + '/' + name
+        return self._get(url)
 
+    def pipelines(self, project: str, pipeline_id=None, ref=None):
+        url = _project_url(project) + '/pipelines'
+        if pipeline_id:
+            url = url + '/' + pipeline_id
+        return self._get(url, ref=ref)
+
+    def pipeline_jobs(self, project: str, pipeline_id: str):
+        url = _project_url(project) + '/pipelines/{0}/jobs'.format(pipeline_id)
         return self._get(url)
--- gitlabcli/run.py
+++ gitlabcli/run.py
@@ -20,6 +20,16 @@ def tags(api: GitlabAPI, args):
     print_json(data)
 
 
+def pipelines(api: GitlabAPI, args):
+    data = api.pipelines(args.project, args.id, args.ref)
+    print_json(data)
+
+
+def pipeline_jobs(api: GitlabAPI, args):
+    data = api.pipeline_jobs(args.project, args.id)
+    return print_json(data)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Access gitlab API')
 
@@ -29,17 +39,28 @@ def main():
 
     subparsers = parser.add_subparsers()
 
-    subparser = subparsers.add_parser('commits', help='Access commits from a repository', parents=[common_parser])
+    subparser = subparsers.add_parser('commits', help='Get commits from a repository', parents=[common_parser])
     subparser.add_argument('project', help='The project id or path (i.e. 1 or namespace/project)')
     subparser.add_argument('-r', '--ref_name', help='The name of a repository branch or tag or if not given the default branch')
     subparser.add_argument('-s', '--since', help='Only commits after or on this date will be returned in ISO 8601 format YYYY-MM-DDTHH:MM:SSZ')
     subparser.add_argument('-u', '--until', help='Only commits before or on this date will be returned in ISO 8601 format YYYY-MM-DDTHH:MM:SSZ')
     subparser.set_defaults(func=commits)
 
-    subparser = subparsers.add_parser('tags', help='Access tags from a repository', parents=[common_parser])
+    subparser = subparsers.add_parser('tags', help='Get tags from a repository', parents=[common_parser])
     subparser.add_argument('project', help='The project id or path (i.e. 1 or namespace/project)')
     subparser.add_argument('-n', '--name', help='The name of the tag')
     subparser.set_defaults(func=tags)
+
+    subparser = subparsers.add_parser('pipelines', help='Get pipelines of a project', parents=[common_parser])
+    subparser.add_argument('project', help='The project id or path (i.e. 1 or namespace/project)')
+    subparser.add_argument('--id', help='The ID of a specific pipeline')
+    subparser.add_argument('-r', '--ref', help='The ref of pipelines')
+    subparser.set_defaults(func=pipelines)
+
+    subparser = subparsers.add_parser('pipeline_jobs', help='Get pipeline jobs of a project', parents=[common_parser])
+    subparser.add_argument('project', help='The project id or path (i.e. 1 or namespace/project)')
+    subparser.add_argument('id', help='The ID of a pipeline')
+    subparser.set_defaults(func=pipeline_jobs)
 
     args = parser.parse_args()
     if hasattr(args, 'func'):
